500530e9f39caed7b4519887f4a656be4d80b84d
waleedf112 <waleedf112@gmail.com>
2020-03-28 01:06:26 +0300

    .

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,17 +13,18 @@ async function sendToUser(token: string, payload: object) {
     await admin.messaging().sendToDevice(token, payload);
 }
 
-async function getUserToken(uid: string) {
-    return await admin.firestore().collection('Users').doc(uid).get().then((snapshot) => {
-        return snapshot.data;
+async function getUserToken(uid: string): Promise<string> {
+    let result: string;
+    await admin.firestore().collection('Users').doc(uid).get().then((snapshot) => {
+        result = snapshot.data().notfificationToken;
     });
+    return result;
 }
 
 export const NewOfferNotification = functions.region('europe-west1').firestore.document('ProductRequests/{doc}/offers/{offer}').onUpdate(async (change, _context) => {
     const newData = change.after.data()
     if (newData) {
-        let token = await getUserToken(newData['traderUid']);
-        console.log(token);
+        let token = await getUserToken(newData.traderUid);
         let payload = {
             notification: {
                 title: 'test',
@@ -33,7 +34,7 @@ export const NewOfferNotification = functions.region('europe-west1').firestore.d
                 click_action: "FLUTTER_NOTIFICATION_CLICK",
             },
         };
-        await sendToUser('', payload).catch((e) => console.log(e));
+        await sendToUser(token, payload).catch((e) => console.log(e));
     }
     return null;
 });
