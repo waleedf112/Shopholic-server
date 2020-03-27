74d66a52b306a2cb97eb88975a10757a76d95660
waleedf112 <waleedf112@gmail.com>
2020-03-28 02:39:57 +0300

    .

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -75,3 +75,52 @@ export const NewOrderNotification = functions.region('europe-west1').firestore.d
     }
     return null;
 });
+
+export const RoleAcceptedNotification = functions.region('europe-west1').firestore.document('Users/{doc}').onUpdate(async (change, _context) => {
+    let { doc } = _context.params;
+    const newData = change.after.data()
+    const oldData = change.before.data()
+
+    if (newData && oldData && oldData.role.pending && !newData.role.pending) {
+        if (oldData.role.requestedRole === newData.role.currentRole) {
+            let roleText: string;
+            if (newData.role.currentRole == 0) roleText = 'مشرف';
+            if (newData.role.currentRole == 1) roleText = 'متسوق شخصي';
+            if (newData.role.currentRole == 2) roleText = 'زبون';
+            await sendToUser(
+                newData.notfificationToken,
+                'تم قبول الترقية',
+                'تم قبول ترقية حسابك الى ' + roleText,
+            ).catch((e) => console.log(e));
+        }
+    }
+    return null;
+});
+
+export const ChatsNotification = functions.region('europe-west1').firestore.document('Chats/{doc}').onWrite(async (change, _context) => {
+    let { doc } = _context.params;
+    const newData = change.after.data()
+
+    if (newData) {
+        let message = newData.messages[newData.messages.length - 1];
+        let person0: string = newData.participantsUids[0];
+        let person0Name: string = newData.participantsNames[0];
+        let person1Name: string = newData.participantsNames[1];
+        let person0Token: string = await getUserToken(newData.participantsUids[0]);
+        let person1Token: string = await getUserToken(newData.participantsUids[1]);
+        if (message.sender == person0) {
+            await sendToUser(
+                person1Token,
+                'رساله من ' + person0Name,
+                message.message,
+            ).catch((e) => console.log(e));
+        } else {
+            await sendToUser(
+                person0Token,
+                'رساله من ' + person1Name,
+                message.message,
+            ).catch((e) => console.log(e));
+        }
+    }
+    return null;
+});
